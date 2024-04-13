10701018794efcc869dbdcdbac43eb8033ea10ac
javlocan <javlocan.dev@gmail.com>
2024-04-13 18:49:27 +0200

    Wichkey update

--- .config/nvim/lua/ui/helpers.lua
+++ .config/nvim/lua/ui/helpers.lua
@@ -9,8 +9,9 @@ return {
       require('which-key').register {
         ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
         ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
+        ['<leader>x'] = { name = 'Tr[X]uble', _ = 'which_key_ignore' },
         ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-        ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
+        ['<leader>f'] = { name = 'Telescope [F]ind', _ = 'which_key_ignore' },
         ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
       }
     end,
