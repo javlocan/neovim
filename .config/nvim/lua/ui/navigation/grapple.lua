43a7d6422905554d6de09f07dde935c295e11350
javlocan <javlocan.dev@gmail.com>
2024-04-14 12:36:12 +0200

    Grapple bindings

--- .config/nvim/lua/ui/navigation/grapple.lua
+++ .config/nvim/lua/ui/navigation/grapple.lua
@@ -8,9 +8,10 @@ return {
     cmd = 'Grapple',
     keys = {
       { 'mm', '<cmd>Grapple toggle<cr>', desc = 'Grapple toggle tag' },
-      -- { '<leader>M', '<cmd>Grapple toggle_tags<cr>', desc = 'Grapple open tags window' },
+      { '<leader>m', '<cmd>Grapple toggle_tags<cr>', desc = 'Grapple open tags window' },
       { 'm]', '<cmd>Grapple cycle_tags next<cr>', desc = 'Grapple cycle next tag' },
       { 'm[', '<cmd>Grapple cycle_tags prev<cr>', desc = 'Grapple cycle previous tag' },
+      { 'mdd', '<cmd>Grapple reset<cr>', desc = 'Grapple cycle previous tag' },
     },
   },
 }
