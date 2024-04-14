732beca8c7763582c5c690424eae727a4b7cd1b7
javlocan <javlocan.dev@gmail.com>
2024-04-14 16:00:16 +0200

    Grapple bindings

--- .config/nvim/lua/ui/navigation/grapple.lua
+++ .config/nvim/lua/ui/navigation/grapple.lua
@@ -7,11 +7,11 @@ return {
     event = { 'BufReadPost', 'BufNewFile' },
     cmd = 'Grapple',
     keys = {
-      { 'mm', '<cmd>Grapple toggle<cr>', desc = 'Grapple toggle tag' },
-      { '<leader>m', '<cmd>Grapple toggle_tags<cr>', desc = 'Grapple open tags window' },
-      { 'm]', '<cmd>Grapple cycle_tags next<cr>', desc = 'Grapple cycle next tag' },
-      { 'm[', '<cmd>Grapple cycle_tags prev<cr>', desc = 'Grapple cycle previous tag' },
-      { 'mdd', '<cmd>Grapple reset<cr>', desc = 'Grapple cycle previous tag' },
+      { 'mm', '<cmd>Grapple toggle scope=git<cr>', desc = 'Grapple toggle tag' },
+      { '<leader>m', '<cmd>Grapple toggle_tags scope=git<cr>', desc = 'Grapple open tags window' },
+      { 'm]', '<cmd>Grapple cycle_tags next scope=git<cr>', desc = 'Grapple cycle next tag' },
+      { 'm[', '<cmd>Grapple cycle_tags prev scope=git<cr>', desc = 'Grapple cycle previous tag' },
+      { 'mdd', '<cmd>Grapple reset scope=git<cr>', desc = 'Grapple cycle previous tag' },
     },
   },
 }
